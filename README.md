89c641ab23e813dab30b4ac536b1d6b583c2de06
malonge <malonge11@gmail.com>
2019-08-22 12:57:43 -0400

    Update README and add plotting of percentage of total sequence.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # PodPlot
-Plot coverage as a function of minimum read length
+Plot coverage or percent of total sequence as a function of minimum read length
+
+![](podplot.cov.png)
+
+![](podplot.perc.png)
+
+
+## Dependencies
+
+- Python3
+- Matplotlib
+
+## Usage
+
+1. Convert fastq to fasta with [seqtk](https://github.com/lh3/seqtk).
+2. Make a fastq index with [samtools](http://www.htslib.org/).
+3. List the fai files:
+
+```
+$ cat fai.fofn
+reads.1.fasta.fai	Shadowfax
+reads.2.fasta.fai	Snowmane	
+reads.3.fasta.fai	Bill
+```
+
+4. `python3 podplot.py fai.fofn` of `python3 podplot.py -g 1500000000 fai.fofn`
--- podplot.py
+++ podplot.py
@@ -14,14 +14,17 @@ def log(message):
 if __name__ == "__main__":
     whale = """\n _V__       _V__       _V__       _V__\n(____\/{   (____\/{   (____\/{   (____\/{\n"""
 
-    print(whale)
-    parser = argparse.ArgumentParser(description='Plot minimum read length vs. coverage for a long read set')
+    parser = argparse.ArgumentParser(description='Plot minimum read length vs. coverage or % sequence. Coverage is plotted when -g is specified. ')
     parser.add_argument("fai", metavar="<fai.fofn>", type=str, help="A list of fai files. First column is the fai file and second column is the label.")
-    parser.add_argument("-g", metavar="[genome size]", type=int, help="expected genome size")
+    parser.add_argument("-g", metavar="<genome_size>", type=int, default=0, help="expected genome size (no prefix symbols).")
+    #parser.add_argument("-y", action='store_true', default=False, help="Instead of covearage, y-axis is percentage of sequence.")
 
     args = parser.parse_args()
     fai_fofn = args.fai
     genome_size = args.g
+    #y = args.y
+
+    print(whale)
 
     # Get all of the fai files
     labels = dict()
@@ -46,28 +49,40 @@ if __name__ == "__main__":
                 read_lens.append(int(rlen))
 
         sorted_lens = sorted(read_lens, reverse=True)
-        covs = dict()
+        seqsums = dict()
         for i in range(len(xvals)):
-            covs[i] = 0
+            seqsums[i] = 0
 
         for i in sorted_lens:
             idx = bisect.bisect_right(xvals, i)
             for j in range(idx):
-                covs[j] += i
+                seqsums[j] += i
 
-        for i in covs:
-            covs[i] = covs[i] / genome_size
+        covs = dict()
+        percs = dict()
 
-        # Plot for this iteration
-        plt.scatter([xvals[i] for i in covs.keys()], covs.values(), s=100, alpha=0.5)
-        plt.plot([xvals[i] for i in covs.keys()], covs.values(), label=labels[fai], linewidth=3, alpha=0.5)
+        if genome_size:
+            for i in seqsums:
+                covs[i] = seqsums[i] / genome_size
+            # Plot for this iteration
+            plt.scatter([xvals[i] for i in covs.keys()], covs.values(), s=100, alpha=0.5)
+            plt.plot([xvals[i] for i in covs.keys()], covs.values(), label=labels[fai], linewidth=3, alpha=0.5)
+        else:
+            total_seq = sum(sorted_lens)
+            for i in seqsums:
+                percs[i] = (seqsums[i] / total_seq) * 100
+            plt.scatter([xvals[i] for i in percs.keys()], percs.values(), s=100, alpha=0.5)
+            plt.plot([xvals[i] for i in percs.keys()], percs.values(), label=labels[fai], linewidth=3, alpha=0.5)
 
     plt.legend(loc=1, prop={'size': 10})
     #plt.rc('axes', labelsize=BIGGER_SIZE)
     #plt.rc('xtick', labelsize=MEDIUM_SIZE)
     #plt.rc('ytick', labelsize=MEDIUM_SIZE)
     plt.xlabel("Min Read Length (bp)")
-    plt.ylabel("Coverage (X)")
+    if genome_size:
+        plt.ylabel("Coverage (X)")
+    else:
+        plt.ylabel("Percentage of Total Sequence")
     plt.grid()
-    plt.savefig("gamplot")
+    plt.savefig("podplot")
     log("Goodbye!")
